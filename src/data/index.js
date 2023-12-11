648586917f79e5b11bf6a90c57af414dfbfba5ca
Phung Xuan Vuong <pxvuong@cmcglobal.vn>
2023-12-11 07:10:25 +0700

    clean

--- src/data/index.js
+++
@@ -1,4 +0,0 @@
-import { ProductsDummyData } from "./ProductsDummyData";
-import { TrendingDummyData } from "./TrendingDummyData";
-
-export { ProductsDummyData, TrendingDummyData }
--- src/screens/index.js
+++
@@ -1,3 +0,0 @@
-import HomeOld from './HomeOld';
-
-export {HomeOld};
